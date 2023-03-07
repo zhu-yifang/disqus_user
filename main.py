b587aed2bcac79599051aafbe62527496071962b
Yifang Zhu <70597936+zhu-yifang@users.noreply.github.com>
2023-03-07 11:46:21 -0800

    added a run function

--- main.py
+++ main.py
@@ -77,35 +77,44 @@ def write_to_csv(user_links):
             f.write(user_link + "\n")
 
 
-with sync_playwright() as p:
-    # launch browser and pretend to be a normal user
-    browser = p.firefox.launch(headless=False)
-    context = browser.new_context(
-        user_agent=
-        'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36'
-    )
-    page = context.new_page()
+def run():
+    with sync_playwright() as p:
+        # launch browser and pretend to be a normal user
+        browser = p.firefox.launch(headless=False)
+        context = browser.new_context(
+            user_agent=
+            'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36'
+        )
+        page = context.new_page()
+        
+        # test with a series
+        user_links = crawl_series(page, "https://fmovies.media/series/the-last-of-us-x1qo8")
 
-    # get all the shows
-    site_url = "https://www.fmovies.media"
-    home_url = site_url + "/home"
-    shows_urls = get_all_shows(page, home_url)
-    for show_url in shows_urls:
-        # if it's a movie
-        if "series" in show_url:
-            user_links = crawl_series(page, site_url + show_url)
-            # remove private users
-            user_links = remove_private(page, user_links)
-            print(user_links)
-            # write to csv
-            write_to_csv(user_links)
-        # if it's a series
-        else:
-            user_links = crawl_a_movie(page, site_url + show_url)
-            # remove private users
-            user_links = remove_private(page, user_links)
-            print(user_links)
-            # write to csv
-            write_to_csv(user_links)
 
-    browser.close()
+        # get all the shows
+        site_url = "https://www.fmovies.media"
+        home_url = site_url + "/home"
+        shows_urls = get_all_shows(page, home_url)
+        for show_url in shows_urls:
+            # if it's a movie
+            if "series" in show_url:
+                user_links = crawl_series(page, site_url + show_url)
+                # remove private users
+                user_links = remove_private(page, user_links)
+                print(user_links)
+                # write to csv
+                write_to_csv(user_links)
+            # if it's a series
+            else:
+                user_links = crawl_a_movie(page, site_url + show_url)
+                # remove private users
+                user_links = remove_private(page, user_links)
+                print(user_links)
+                # write to csv
+                write_to_csv(user_links)
+
+        browser.close()
+
+
+if __name__ == '__main__':
+    run()
